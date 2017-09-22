cd8c903da1bc3297e68f20f5efdf64fd5f3d3fca
carloshrs <carloshrs@a967abec-17ac-4d50-b6c4-48d86383e8d0>
2017-09-22 22:58:50 +0000

    Reporte de Cantidad de informes

--- FrontEnd/Intranet/Admin/Reportes/ReporteCantidadInformes.aspx.cs
+++ FrontEnd/Intranet/Admin/Reportes/ReporteCantidadInformes.aspx.cs
@@ -63,6 +63,16 @@ namespace ar.com.TiempoyGestion.FrontEnd.Intranet.Admin.Reportes
         protected void btnBuscar_Click(object sender, EventArgs e)
         {
             string vTiposInformes = "";
+            int vPeriodo = 1;
+            string FechaDesde2 = "";
+            string FechaHasta2 = "";
+            string FechaDesde3 = "";
+            string FechaHasta3 = "";
+            string FechaDesde4 = "";
+            string FechaHasta4 = "";
+            string FechaDesde5 = "";
+            string FechaHasta5 = "";
+
             if (txtFechaInicio.Text == "")
                 txtFechaInicio.Text = DateTime.Today.AddDays(-60).ToShortDateString();
             FechaDesde = txtFechaInicio.Text;
@@ -81,11 +91,101 @@ namespace ar.com.TiempoyGestion.FrontEnd.Intranet.Admin.Reportes
 
             vTiposInformes = vTiposInformes.Substring(0, vTiposInformes.Length - 1);
 
-
-            ReportesCobranzas rsReportes = new ReportesCobranzas();
+            lblCantidadInformes1.Text = "Informes del " + FechaDesde + " al " + FechaHasta;
+            ReportesCobranzas rsReportes1 = new ReportesCobranzas();
             dgCantidadInformes1.DataSource = ReportesCobranzas.ListarCantidadInformes(FechaDesde, FechaHasta, vTiposInformes, int.Parse(cmbEstado.SelectedValue)).DefaultView;
             dgCantidadInformes1.DataBind();
+
+            if (int.Parse(cmbPeriodo.SelectedValue) == 1) // Meses 1, Años 2
+                vPeriodo = 1;
+            else
+                vPeriodo = 2;
+
+            if (int.Parse(cmbCantidad.SelectedValue) >= 2)
+            {
+                pnPanel2.Visible = true;
+                if (vPeriodo == 1)
+                {
+                    FechaDesde2 = DateTime.Parse(FechaDesde).AddMonths(-1).ToShortDateString();
+                    FechaHasta2 = DateTime.Parse(FechaHasta).AddMonths(-1).ToShortDateString();
+                }
+                else
+                {
+                    FechaDesde2 = DateTime.Parse(FechaDesde).AddYears(-1).ToShortDateString();
+                    FechaHasta2 = DateTime.Parse(FechaHasta).AddYears(-1).ToShortDateString();
+                }
+
+                lblCantidadInformes2.Text = "Informes del " + FechaDesde2 + " al " + FechaHasta2;
+                ReportesCobranzas rsReportes2 = new ReportesCobranzas();
+                dgCantidadInformes2.DataSource = ReportesCobranzas.ListarCantidadInformes(FechaDesde, FechaHasta, vTiposInformes, int.Parse(cmbEstado.SelectedValue)).DefaultView;
+                dgCantidadInformes2.DataBind();
+            }
+
+
+            if (int.Parse(cmbCantidad.SelectedValue) >= 3)
+            {
+                pnPanel3.Visible = true;
+                if (vPeriodo == 1)
+                {
+                    FechaDesde3 = DateTime.Parse(FechaDesde).AddMonths(-2).ToShortDateString();
+                    FechaHasta3 = DateTime.Parse(FechaHasta).AddMonths(-2).ToShortDateString();
+                }
+                else
+                {
+                    FechaDesde3 = DateTime.Parse(FechaDesde).AddYears(-2).ToShortDateString();
+                    FechaHasta3 = DateTime.Parse(FechaHasta).AddYears(-2).ToShortDateString();
+                }
+
+                lblCantidadInformes3.Text = "Informes del " + FechaDesde3 + " al " + FechaHasta3;
+                ReportesCobranzas rsReportes3 = new ReportesCobranzas();
+                dgCantidadInformes3.DataSource = ReportesCobranzas.ListarCantidadInformes(FechaDesde, FechaHasta, vTiposInformes, int.Parse(cmbEstado.SelectedValue)).DefaultView;
+                dgCantidadInformes3.DataBind();
+            }
+
+
+
+            if (int.Parse(cmbCantidad.SelectedValue) >= 4)
+            {
+                pnPanel4.Visible = true;
+                if (vPeriodo == 1)
+                {
+                    FechaDesde4 = DateTime.Parse(FechaDesde).AddMonths(-3).ToShortDateString();
+                    FechaHasta4 = DateTime.Parse(FechaHasta).AddMonths(-3).ToShortDateString();
+                }
+                else
+                {
+                    FechaDesde4 = DateTime.Parse(FechaDesde).AddYears(-3).ToShortDateString();
+                    FechaHasta4 = DateTime.Parse(FechaHasta).AddYears(-3).ToShortDateString();
+                }
+
+                lblCantidadInformes4.Text = "Informes del " + FechaDesde4 + " al " + FechaHasta4;
+                ReportesCobranzas rsReportes4 = new ReportesCobranzas();
+                dgCantidadInformes4.DataSource = ReportesCobranzas.ListarCantidadInformes(FechaDesde, FechaHasta, vTiposInformes, int.Parse(cmbEstado.SelectedValue)).DefaultView;
+                dgCantidadInformes4.DataBind();
+            }
+
+
+            if (int.Parse(cmbCantidad.SelectedValue) == 5)
+            {
+                pnPanel5.Visible = true;
+                if (vPeriodo == 1)
+                {
+                    FechaDesde5 = DateTime.Parse(FechaDesde).AddMonths(-4).ToShortDateString();
+                    FechaHasta5 = DateTime.Parse(FechaHasta).AddMonths(-4).ToShortDateString();
+                }
+                else
+                {
+                    FechaDesde5 = DateTime.Parse(FechaDesde).AddYears(-4).ToShortDateString();
+                    FechaHasta5 = DateTime.Parse(FechaHasta).AddYears(-4).ToShortDateString();
+                }
+
+                lblCantidadInformes5.Text = "Informes del " + FechaDesde5 + " al " + FechaHasta5;
+                ReportesCobranzas rsReportes5 = new ReportesCobranzas();
+                dgCantidadInformes5.DataSource = ReportesCobranzas.ListarCantidadInformes(FechaDesde, FechaHasta, vTiposInformes, int.Parse(cmbEstado.SelectedValue)).DefaultView;
+                dgCantidadInformes5.DataBind();
+            }
         }
+
 
 
         private void ListaTiposInformes()
